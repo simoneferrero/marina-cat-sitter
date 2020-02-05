8a288f3e6be9287a101fc7df19a1178f465cc626
Simone Ferrero <simone.ferrero@outlook.com>
2020-02-05 00:15:56 +0000

    Modified packages descriptions

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,19 +3,19 @@ export const packages = [
 		id: '1 passaggio',
 		title: '1 passaggio',
 		text:
-			'Una visita breve per dare da mangiare, pulire la sabbietta e assicurarti che tutto sia a posto',
+			'Una visita giornaliera per nutrire, pulire, coccolare e assicurarti che tutto sia a posto.',
 	},
 	{
 		id: '2 passaggi',
 		title: '2 o più passaggi',
 		text:
-			'Una visita più lunga per dare da mangiare, pulire la sabbietta e fare compagnia ai tuoi animali',
+			'Per fare sentire I tuoi amici meno soli, con due o più visite giornaliere.',
 		favourite: true,
 	},
 	{
 		id: 'personalizzato',
 		title: 'personalizzato',
 		text:
-			'Nel caso i tuoi animali necessitino di cure particolari o di una compagnia prolungata',
+			'Nel caso I tuoi animali necessitino di cure particolari o di una compagnia prolungata.',
 	},
 ]
